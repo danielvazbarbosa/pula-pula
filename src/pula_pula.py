0337d729f35c4f38477da7835adb6de21f92ccec
Daniel <danielvaz@alu.ufc.br>
2023-10-23 22:42:20 -0300

    first commit

---
+++ src/pula_pula.py
@@ -0,0 +1,35 @@
+from crianca import Crianca
+
+class PulaPula:
+
+    def __init__(self, limiteMax):
+        pass
+    def getFilaDeEspera(self):
+        return None
+
+    def getCriancasPulando(self):
+        return None
+
+    def getLimiteMax(self):
+        return -1
+
+    def getCaixa(self):
+        return -1
+
+    def getConta(self, nome):
+        return None
+
+    def entrarNaFila(self, crianca: Crianca):
+        return True
+
+    def entrar(self):
+        return True
+
+    def sair(self):
+        return True
+
+    def papaiChegou(self, nome):
+        return False
+
+    def fechar(self):
+        return -1
